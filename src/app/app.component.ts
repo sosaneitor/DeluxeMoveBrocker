9d074ae0971d40de9b61f03d33c843115fb73f68
sosaneitor <sss_sosa@hotmail.com>
2025-04-10 19:43:23 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [LandingPageComponent],
+  template: `<app-landing-page />`,
 })
-export class AppComponent {
-  title = 'car-transport-landing';
-}
+export class AppComponent {}
